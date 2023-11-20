2d7243d92894306ab510ff79b5fc191af7b29200
czyk <aron.scheffczyk@gmail.com>
2023-11-20 11:17:37 +0100

    add switch component

--- src/Comps/SwitchComp.tsx
+++ src/Comps/SwitchComp.tsx
@@ -1,15 +1,44 @@
-import React, { useState } from 'react';
-import { Switch, SwitchProps } from '@mui/material';
-import './Switch.scss'; 
+import React, { useState } from "react";
+import { Switch, SwitchProps } from "@mui/material";
+import "./Switch.scss";
 
-
-interface SwitchComp {
-    initialActive?: boolean;
-    onToggle?: (active: boolean) => void;
+interface SwitchComp extends SwitchProps {
+  initialActive?: boolean;
+  onToggle?: (active: boolean) => void;
+  labelOn?: JSX.Element | string
+  labelOff?: JSX.Element | string
 }
 
-export const SwitchComp = (props: SwitchProps) => {
-    return (
-        <Switch {...props} />
-    );
+export const SwitchComp = (props: SwitchComp) => {
+  const { initialActive = false, onToggle, labelOn = "", labelOff = "" } = props;
+
+  const [isChecked, setIsChecked] = useState(initialActive);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+    if (onToggle) {
+      onToggle(event.target.checked);
+    }
+  };
+
+  return (
+    <div className="switch">
+      <Switch
+        {...props}
+        sx={{
+          "&.MuiSwitch-root .MuiSwitch-switchBase": {
+            color: "var(--secondary)",
+          },
+          "&.MuiSwitch-root .Mui-checked": {
+            color: "var(--action)",
+          },
+          "&.MuiSwitch-root .MuiSwitch-track": {
+            backgroundColor: "var(--textColor)",
+          },
+        }}
+        onChange={handleChange}
+      />
+      <label style={{fontSize: '2rem'}}>{isChecked ? labelOn : labelOff}</label>
+    </div>
+  );
 };
